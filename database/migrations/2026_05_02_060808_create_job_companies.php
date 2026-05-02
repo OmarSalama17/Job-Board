0a7fafcac68b116e6ac9d119be52d53743bdebe9
OmarSalama17 <o.salama.abdallah@gmail.com>
2026-05-02 09:45:44 +0300

    edit migrations

--- database/migrations/2026_05_02_060808_create_job_companies.php
+++ database/migrations/2026_05_02_060808_create_job_companies.php
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('job_categories');
+        Schema::dropIfExists('job_companies');
     }
 };
--- database/migrations/2026_05_02_061943_create_job_resumes.php
+++ database/migrations/2026_05_02_061943_create_job_resumes.php
@@ -33,6 +33,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('job_categories');
+        Schema::dropIfExists('job_resumes');
     }
 };
